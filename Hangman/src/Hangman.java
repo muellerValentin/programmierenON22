24173a7fc4bba627d5b390181256036104e99837
muellerValentin <mueller.valentin@icloud.com>
2022-12-16 10:25:24 +0100

    Erster Test: Auswahl Spielmodus
    Switch Case zur Auswahl eines Spielmodus

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -3,8 +3,27 @@ import java.util.Scanner;
 public class Hangman {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		Scanner scan = new Scanner(System.in);
+		// Abfrage: Welcher Spielmodus soll gestartet werden?
+		// Spielmodus 1: Gegen den Computer (mit vorgegebenen Wörtern).
+		// Spielmodus 2: Wort wird selbst eingegeben und dann gespielt.
+		
+		System.out.println("Das Spiel Hangman wurde gestartet! \n"
+				+ "Spielmodus 1: Spiele gegen den Computer \n"
+				+ "Spielmodus 2: Gib ein eigenes Wort ein (Multiplayer) \n"
+				+ "\n"
+				+ "Wähle den Spielmodus aus: Tippe 1 oder 2");
+		
+		int spielmodus = scan.nextInt();
+		
+		switch(spielmodus) {
+			case 1 -> System.out.println("Es wurde Spielmodus 1 gewählt.");
+			case 2 -> System.out.println("Es wurde Spielmodus 2 gewählt.");
+			default -> System.out.println("Falsche Eingabe!");
+		}
+		
+		
 		System.out.println("Wort vorschlag eingeben");
 		String input = scan.nextLine();
 		Spiel gg = new Spiel(input);

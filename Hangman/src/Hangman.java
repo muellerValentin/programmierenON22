e30808ffbca0116d61091f198626f0beae9240af
muellerValentin <mueller.valentin@icloud.com>
2022-12-16 12:18:39 +0100

    Aufteilung des Spiels in Spielmodus 1 und Spielmodus 2

--- Hangman/src/Hangman.java
+++ Hangman/src/Hangman.java
@@ -1,34 +1,75 @@
 import java.util.Scanner;
+import java.util.Random;
 
 public class Hangman {
 
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
+		
+		/////////////////////////////////////////////////////////
+		///////////////// Abfrage Spielmodus ////////////////////
+		/////////////////////////////////////////////////////////
+		
 		// Abfrage: Welcher Spielmodus soll gestartet werden?
 		// Spielmodus 1: Gegen den Computer (mit vorgegebenen Wörtern).
 		// Spielmodus 2: Wort wird selbst eingegeben und dann gespielt.
 		
 		System.out.println("Das Spiel Hangman wurde gestartet! \n"
+				+ "\n"
 				+ "Spielmodus 1: Spiele gegen den Computer \n"
 				+ "Spielmodus 2: Gib ein eigenes Wort ein (Multiplayer) \n"
 				+ "\n"
 				+ "Wähle den Spielmodus aus: Tippe 1 oder 2");
 		
 		int spielmodus = scan.nextInt();
+		String xx = scan.nextLine();		// Wird benötigt, damit keine Fehlermeldung kommt
+		String input = "";
 		
 		switch(spielmodus) {
-			case 1 -> System.out.println("Es wurde Spielmodus 1 gewählt.");
-			case 2 -> System.out.println("Es wurde Spielmodus 2 gewählt.");
+			case 1 -> {
+				/////////////////////////////////////////////////////////
+				//////////////////// Spielmodus 1 ///////////////////////
+				/////////////////////////////////////////////////////////
+				
+				System.out.println("Es wurde Spielmodus 1 gewählt.");
+			
+				// Vorgegebene Wörter für Spielmodus 1
+				String[] woerter = {"Christbaumkugel", "Weihnachten", "Rentierschlitten", "Glühwein", "Heiligabend"};
+				
+				// Zufällige Zahl für die Wortauswahl
+				int min = 0;
+				int max = 4;
+				
+				Random random = new Random();
+				
+				int randomNumber = random.nextInt(max + min) + min;
+				System.out.println(randomNumber);
+				
+				System.out.println(woerter[randomNumber]);
+				
+				// Die Variable input wird auf das zufällig gewählte Wort gesetzt
+				input = woerter[randomNumber];
+			}
+			case 2 -> {
+				/////////////////////////////////////////////////////////
+				//////////////////// Spielmodus 2 ///////////////////////
+				/////////////////////////////////////////////////////////
+				
+				System.out.println("Es wurde Spielmodus 2 gewählt.\n \n");
+				
+				System.out.println("Gib ein zu erratendes Wort ein:");
+				// Die Variable input wird auf das eingegebene Wort gesetzt
+				input = scan.nextLine();
+			}
 			default -> System.out.println("Falsche Eingabe!");
 		}
-		
-		
-		System.out.println("Wort vorschlag eingeben");
-		String input = scan.nextLine();
+	
 		Spiel gg = new Spiel(input);
+		
 		char inChar;
 		input = "Start";
+		
 		for(int i=0; i<60;i++) {
 	        System.out.println();
 		}
